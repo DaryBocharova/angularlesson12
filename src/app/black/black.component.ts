2faefc9dcdb0b12db5e1d0dfaceb52b8fa4ee85a
d.bocharova <d.bocharova@x5.ru>
2020-08-31 14:34:36 +0300

    lesson12

---
+++ src/app/black/black.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-black',
+  template: `
+  <div class="black">`,
+  styles: [`
+  .black{
+    background: #000000;
+    wigth: 30px;
+    height: 80px;
+  }`]
+})
+export class BlackComponent{
+}
---
+++ src/app/white/white.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-white',
+  template: `<div class="white">`,
+  styles: [`
+  .white{
+    background: #f0d9b5; 
+    wigth: 30px;
+    height: 80px;
+  }`]
+})
+  export class WhiteComponent {
+
+}
